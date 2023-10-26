4b78199f727ae2af183d856140b8a4289c312b21
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-10-26 16:40:15 +0200

    ex completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return 'ERROR'
+  } else {
+    const repetition = [];
+    for (let i = 0; i < num; i++) {
+      repetition.push(string)
+    }
+    return repetition.join("")
+  }
 };
 
 // Do not edit below this line
