d866b9b7b87986084c0ca163df72d054c88f5a71
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-10-27 15:45:49 +0200

    ex completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  return arr.filter(x => !args.includes(x))
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// 1. Sélectionner l'index de l'argument
+// 2. Supprimer la valeur à cet index
+// 3. Retourner l'arrêt sans cette valeur
