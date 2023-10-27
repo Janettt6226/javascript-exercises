4fc713b6c77ebf4bf863c816cbfdd96bc477ddf5
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-10-27 17:38:29 +0200

    ex completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let sum = 0;
+  if (min < 0 || max < 0 || typeof(min) != "number" || typeof(max) != "number") {
+    return "ERROR"
+  } else {
+    if (min > max) {
+      for (let i= max; i <= min; i++) {
+        sum += i
+      }
+    } else {
+      for (let i= min; i <= max; i++) {
+        sum += i
+      }
+    }
+    return sum
 
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// arg1 et arg2
+// return sum = arg1 + arg + arg2
